cc332aa5287eb4d9244b51a5514889ceed43404d
jy <thexl74@gmail.com>
2020-11-21 20:23:19 +0900

    Added model summaries

---
+++ make_model_summary.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import numpy
+import glob
+import os
+import tensorflow as tf
+from sklearn.metrics import f1_score
+import keras
+
+model_paths = glob.glob('*/*.h5')
+
+f1_model_pairs = []
+for model_path in model_paths:
+    prefix = os.path.dirname(model_path)
+    result_path = os.path.join(prefix, 'result.txt')
+    if not os.path.exists(result_path):
+        print('Training not completed: {}'.format(prefix))
+        continue
+
+    with open(result_path) as result_file:
+        f1 = float(result_file.readline().strip())
+        print('Loading {}'.format(model_path))
+        model = keras.models.load_model(model_path)
+        f1_model_pairs.append((f1, model_path, model))
+
+f1_model_pairs = sorted(f1_model_pairs)[::-1]
+
+for f1, model_path, model in f1_model_pairs[:10]:
+    print(model_path)
+    print(f1)
+    model.summary()
+    print()
+    print()
